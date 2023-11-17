de1b91345e711efb2372a3482654593ded511e8c
juzeon <812312770@qq.com>
2023-11-17 10:46:59 +0800

    add utils

---
+++ mock-server/util/util.go
@@ -0,0 +1,15 @@
+package util
+
+import "math/rand"
+
+func RandIntInclusive(min int, max int) int {
+	return min + rand.Intn(max-min+1)
+}
+func RandStringRunes(n int) string {
+	letterRunes := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
